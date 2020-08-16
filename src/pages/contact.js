ae0e12505ec251300862c20977b16e45c8aeb7bd
Rui Onodera <deraru@gmail.com>
2020-08-16 15:48:05 +0900

    Add contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,6 +7,21 @@ const SecondPage = () => (
   <Layout>
     <SEO title="お問い合わせ" />
     <h1>お問い合わせはこちらからお願いします。</h1>
+    <form name="お問い合わせフォーム" method="POST" data-netlify="true">
+      <input type="hidden" name="form-name" value="お問い合わせフォーム" />
+      <p>
+        <label>お名前: <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>メールアドレス: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>お問い合わせ内容: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">送信する</button>
+      </p>
+    </form>
   </Layout>
 )
 

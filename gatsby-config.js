6f8ecb533a6599dc4f4a7b3b62f151acde221c5a
Rui Onodera <deraru@gmail.com>
2020-08-31 14:27:14 +0900

    Add icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,13 +27,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Maiko`,
+        short_name: `Maiko`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: `#ffffff`,
+        theme_color: `#ffffff`,
+        display: `Maiko`,
+        icon: `src/images/logo.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality

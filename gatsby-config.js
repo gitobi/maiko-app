07359c8b04a87041ae387c6bfda4c7426d4cdc95
Rui Onodera <deraru@gmail.com>
2021-08-04 23:09:03 +0900

    Add mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,8 +37,16 @@ module.exports = {
         icon: `src/images/logo.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        extensions: [".mdx", ".md"],
+        defaultLayouts: {
+          default: require.resolve(
+            "./src/components/templates/base-layout.js"
+          ),
+        },
+      },
+    },
   ],
 }

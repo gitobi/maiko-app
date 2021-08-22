e265e6d8925129e1359f940fec395a9eb24ebc9d
Rui Onodera <deraru@gmail.com>
2021-08-22 20:15:00 +0900

    Add total quantity

--- src/components/organisms/footer.js
+++ src/components/organisms/footer.js
@@ -1,6 +1,7 @@
-import React from "react"
+import React, { useContext } from "react"
 import { Link } from "gatsby"
 
+import { StoreContext } from "@gitobi/gitobi-shopify-context"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
   faHome,
@@ -18,48 +19,53 @@ import {
   footerCopyRightLink,
 } from "./footer.module.scss"
 
-const Footer = () => (
-  <footer className={footer}>
-    <nav>
-      <ul className={footerNav}>
-        <li className={footerNavItem}>
-          <Link className={footerNavItemLink} to="/">
-            <FontAwesomeIcon className={footerNavItemLinkIcon} icon={faHome} />
-            <span className={footerNavItemLinkText}> ホーム</span>
-          </Link>
-        </li>
-        <li className={footerNavItem}>
-          <Link className={footerNavItemLink} to="/shop">
-            <FontAwesomeIcon
-              className={footerNavItemLinkIcon}
-              icon={faStore}
-            />
-            <span className={footerNavItemLinkText}> ストア</span>
-          </Link>
-        </li>
-        <li className={footerNavItem}>
-          <Link className={footerNavItemLink} to="/cart">
-            <FontAwesomeIcon
-              className={footerNavItemLinkIcon}
-              icon={faShoppingCart}
-            />
-            <span className={footerNavItemLinkText}> カート</span>
-          </Link>
-        </li>
-      </ul>
-    </nav>
-    <p className={footerCopyRight}>
-      &copy; 2020&nbsp;
-      <a
-        className={footerCopyRightLink}
-        href="https://www.gitobi.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Gitobi LLC.
-      </a>
-    </p>
-  </footer>
-)
+const Footer = () => {
+  const { currentTotalQuantity } = useContext(StoreContext)
+
+  return (
+    <footer className={footer}>
+      <nav>
+        <ul className={footerNav}>
+          <li className={footerNavItem}>
+            <Link className={footerNavItemLink} to="/">
+              <FontAwesomeIcon className={footerNavItemLinkIcon} icon={faHome} />
+              <span className={footerNavItemLinkText}> ホーム</span>
+            </Link>
+          </li>
+          <li className={footerNavItem}>
+            <Link className={footerNavItemLink} to="/shop">
+              <FontAwesomeIcon
+                className={footerNavItemLinkIcon}
+                icon={faStore}
+              />
+              <span className={footerNavItemLinkText}> ストア</span>
+            </Link>
+          </li>
+          <li className={footerNavItem}>
+            <Link className={footerNavItemLink} to="/cart">
+              <FontAwesomeIcon
+                className={footerNavItemLinkIcon}
+                icon={faShoppingCart}
+              />
+              <span className={footerNavItemLinkText}> カート</span>
+              <span> {currentTotalQuantity()}</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <p className={footerCopyRight}>
+        &copy; 2020&nbsp;
+        <a
+          className={footerCopyRightLink}
+          href="https://www.gitobi.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Gitobi LLC.
+        </a>
+      </p>
+    </footer>
+  )
+}
 
 export default Footer
--- src/components/organisms/header.js
+++ src/components/organisms/header.js
@@ -1,6 +1,7 @@
-import React from "react"
+import React, { useContext } from "react"
 import { Link } from "gatsby"
 
+import { StoreContext } from "@gitobi/gitobi-shopify-context"
 import logo from "../../images/logo.png"
 import font from "../../images/font.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
@@ -19,34 +20,38 @@ import {
   headerNavItemLink,
 } from "./header.module.scss"
 
-const Header = () => (
-  <header className={header}>
-    <div className={topLinkContainer}>
-      <Link to="/">
-        <img className={siteLogo} src={logo} alt="Maiko logo" />
-        <img className={siteFont} src={font} alt="Maiko" />
-      </Link>
-    </div>
-    <nav>
-      <ul className={headerNav}>
-        <li className={headerNavItem}>
-          <Link className={headerNavItemLink} to="/">
-            <FontAwesomeIcon icon={faHome} /> ホーム
-          </Link>
-        </li>
-        <li className={headerNavItem}>
-          <Link className={headerNavItemLink} to="/shop">
-            <FontAwesomeIcon icon={faStore} /> ストア
-          </Link>
-        </li>
-        <li className={headerNavItem}>
-          <Link className={headerNavItemLink} to="/cart">
-            <FontAwesomeIcon icon={faShoppingCart} /> カート
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-)
+const Header = () => {
+  const { currentTotalQuantity } = useContext(StoreContext)
+
+  return (
+    <header className={header}>
+      <div className={topLinkContainer}>
+        <Link to="/">
+          <img className={siteLogo} src={logo} alt="Maiko logo" />
+          <img className={siteFont} src={font} alt="Maiko" />
+        </Link>
+      </div>
+      <nav>
+        <ul className={headerNav}>
+          <li className={headerNavItem}>
+            <Link className={headerNavItemLink} to="/">
+              <FontAwesomeIcon icon={faHome} /> ホーム
+            </Link>
+          </li>
+          <li className={headerNavItem}>
+            <Link className={headerNavItemLink} to="/shop">
+              <FontAwesomeIcon icon={faStore} /> ストア
+            </Link>
+          </li>
+          <li className={headerNavItem}>
+            <Link className={headerNavItemLink} to="/cart">
+              <FontAwesomeIcon icon={faShoppingCart} /> カート {currentTotalQuantity()}
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
 
 export default Header
